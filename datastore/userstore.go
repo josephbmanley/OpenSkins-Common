eea81c7820d0605b94f27a1b20dd8847795242fa
Joseph Manley <j@cloudsumu.com>
2021-01-29 18:55:49 -0500

    Change set user type

--- datastore/userstore.go
+++ datastore/userstore.go
@@ -5,5 +5,5 @@ import "github.com/josephbmanley/OpenSkins-Common/datatypes"
 type Userstore interface {
 	Initialize() error
 	GetUser(uid string) (*datatypes.User, error)
-	SetUser(datatypes.User) error
+	SetUser(*datatypes.User) error
 }
